ad62d9dab8d57db202cf04df83f846b9cfc23272
nitesh <nitesh@nitesh.(none)>
2014-06-16 21:01:35 +0530

    4

--- rssreader/static/js/feed.js
+++ rssreader/static/js/feed.js
@@ -12,14 +12,31 @@ window.onload=function(){
     }).done(function(data) {
 
          for(var i=0;i<data.count;i++){
+         
+           img="<img src=\""+data.dat[i].image+"\" />";
+           console.log(img);
+           colst1="<div class=\"col-md-1\">"
+           colst2="<div class=\"col-md-2\">";
+           colst3="<div class=\"col-md-3\">";
+           colst4="<div class=\"col-md-4\">";
+           
+           sh=colst1+"<a href=\""+data.dat[i].link+"\">"+img+"</a></div>";
+           console.log(sh);
+           
            if(i%3==0)
              $('#result').append("<div class=\"row\" id=\"row"+i+"\">");
              
-           $('#row'+i).append("<div class=\"col-md-4\">"+data.dat[i].title+"</div>");
+           $('#row'+i).append(colst3+"<b>"+data.dat[i].title+"</b>"+"</div>");
            
-           $('#row'+i).append("<div class=\"col-md-4\">"+"<a href=\""+data.dat[i].link+"\">link</a>"+"</div>");
            
-           $('#row'+i).append("<div class=\"col-md-4\">"+data.dat[i].summary+"</div>");
+           $('#row'+i).append(sh);
+           
+           $('#row'+i).append(colst4+data.dat[i].summary+"</div>");
+          
+           
+           
+           //$('#row'+i).append("<div class=\"col-md-4\">"+data.dat[i].summary+"</div>");
+           
              
            
            if(i%3==0)
